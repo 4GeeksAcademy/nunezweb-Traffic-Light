2d3452275188ebf09c754331a7f64cefd28f2d99
Alejandro Núñez <145801334+nunezweb@users.noreply.github.com>
2024-06-11 08:31:22 +0000

    Traffic lights game with mouse over

---
+++ src/js/component/TrafficLight.jsx
@@ -0,0 +1,97 @@
+import React, { useState } from "react";
+
+const TrafficLight = () => {
+  const [shadowRed, setShadowRed] = useState("none");
+  const [shadowYellow, setShadowYellow] = useState("none");
+  const [shadowGreen, setShadowGreen] = useState("none");
+  const [yellowLigthToPurple, setYellowLigthToPurple] = useState("yellow");
+
+  const shadowRedColor = (bgColor) => {
+    setShadowRed(bgColor);
+  };
+  const shadowYellowColor = (bgColor) => {
+    setShadowYellow(bgColor);
+  };
+  const shadowGreenColor = (bgColor) => {
+    setShadowGreen(bgColor);
+  };
+  const yellowLigthChange = (LigthColor) => {
+    setYellowLigthToPurple(LigthColor);
+  };
+
+  return (
+    <div className="container text-center p-5">
+      <div className="row justify-content-center d-flex flex-column align-items-center">
+        <div className="col-auto bg-dark text-white rounded-top p-3 d-flex flex-column">
+          <div
+            style={{
+              backgroundColor: "red",
+              width: "120px",
+              height: "120px",
+              boxShadow: shadowRed,
+            }}
+            className="rounded-circle"
+            onMouseEnter={() => shadowRedColor("0 0 18px yellow")}
+            onMouseOut={() => shadowRedColor("none")}
+            onClick={() =>
+              alert("No, no, the red color is fine! Try another one...")
+            }
+          ></div>
+        </div>
+        <div className="col-auto bg-dark text-white p-3 d-flex flex-column">
+          <div
+            style={{
+              backgroundColor: yellowLigthToPurple,
+              width: "120px",
+              height: "120px",
+              boxShadow: shadowYellow,
+            }}
+            className="rounded-circle yellowLigth"
+            onMouseEnter={() => shadowYellowColor("0 0 18px yellow")}
+            onMouseOut={() => shadowYellowColor("none")}
+            onClick={() => yellowLigthChange("yellow")}
+          ></div>
+        </div>
+        <div className="col-auto bg-dark text-white rounded-bottom p-3 d-flex flex-column">
+          <div
+            style={{
+              backgroundColor: "green",
+              width: "120px",
+              height: "120px",
+              boxShadow: shadowGreen,
+            }}
+            className="rounded-circle"
+            onMouseEnter={() => shadowGreenColor("0 0 18px yellow")}
+            onMouseOut={() => shadowGreenColor("none")}
+            onClick={() =>
+              alert("No, no, the green color is fine! Try another one...")
+            }
+          ></div>
+        </div>
+      </div>
+      <div
+        className="mt-3 mx-2 pt-3 btn btn-warning"
+        onClick={() =>
+          alert("No, this is not it, but you're on the right track...")
+        }
+      >
+        <p>Discover hidden features.</p>
+      </div>
+      <div
+        className="mt-3 mx-2 pt-3 btn btn-warning"
+        onClick={() =>
+          yellowLigthChange(
+            "#800080",
+            alert(
+              "Hey!!! Traffic lights don't have this color, try to restore the color without reloading the page."
+            )
+          )
+        }
+      >
+        <p>Purple Ligth?</p>
+      </div>
+    </div>
+  );
+};
+
+export default TrafficLight;
